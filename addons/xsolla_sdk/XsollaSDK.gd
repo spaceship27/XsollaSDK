7e9d4878d7508ad2bd0db2122ae5bc419f3b2919
Adrian <76214766+adrian2793@users.noreply.github.com>
2024-03-08 13:39:27 +0100

    Update XsollaSDK.gd
    Signed-off-by: Adrian <76214766+adrian2793@users.noreply.github.com>

--- addons/xsolla_sdk/XsollaSDK.gd
+++ addons/xsolla_sdk/XsollaSDK.gd
@@ -1,14 +1,13 @@
 @tool
 extends HTTPRequest
 
-var json_result;
 var json;
 var _request_completed : bool = false;
 
 func get_json():
-	return json;
+	return json.data;
 
-func get_virtual_items(project_id : int, limit : int = 50, offset : int = 0, locale : String = "en", show_inactive_time_limited_offer : int = 0):
+func get_virtual_items_list(project_id : int, limit : int = 50, offset : int = 0, locale : String = "en", show_inactive_time_limited_offer : int = 0):
 	$HTTPRequest.request("https://store.xsolla.com/api/v2/project/" + str(int(project_id)) + "/items/virtual_items?limit=" + str(int(limit)) + "&offset=" + str(int(offset)) + "&locale=" + locale + "&show_inactive_time_limited_items=" + str(int(show_inactive_time_limited_offer)));
 	$HTTPRequest.request_completed.connect(_on_request_completed);
 	
@@ -16,4 +15,5 @@ func _on_request_completed(result, response_code, headers, body):
 	var json = JSON.parse(body.get_string_from_utf8());
 	var _request_completed = true;
 	print(_request_completed);
+	print("----------");
 	print(json);
