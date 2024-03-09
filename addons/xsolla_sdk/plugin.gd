d3b8cba40c6c310c7e6bd87694bc8f237c8beb2d
Adrian <76214766+adrian2793@users.noreply.github.com>
2024-03-09 07:29:33 +0100

    Delete addons/xsolla_sdk directory
    Signed-off-by: Adrian <76214766+adrian2793@users.noreply.github.com>

--- addons/xsolla_sdk/plugin.gd
+++
@@ -1,8 +0,0 @@
-@tool
-extends EditorPlugin
-
-func _enter_tree():
-	add_custom_type("XsolllaSDK", "HTTPRequest", preload("XsollaSDK.gd"), preload("node.svg"));
-
-func _exit_tree():
-	remove_custom_type("XsollaSDK");
